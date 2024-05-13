32f96e208cf9df8b8a5c4839e98593f5d4d0cc58
WIZIRAT <ni.kb@yandex.com>
2024-05-13 09:49:35 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.games.even import brain_even
+from brain_games.games.even import start_game_even
 
 
 def main():
-    brain_even()
+    start_game_even()
 
 
 if __name__ = '__main__':
