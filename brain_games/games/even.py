bc4ecfb3942246c0bcf8d963dae4065e8912a711
WIZIRAT <ni.kb@yandex.com>
2024-05-13 09:38:40 +0300

    fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,13 @@
+from brain_games.core import even_game
 import random
+from brain_games.instruction import INSTRUCTION_EVEN
+
 
 def get_question_answer():
     question = random.randint(1, 100)
     result = 'yes' if question % 2 == 0 else 'no'
-    return question. result
+    return question, result
+
+
+def start_game_even():
+    game_even(get_question_answer, INSTRUCTION_EVEN)
