22df1090db86a27356b46a4376d22d7cac2bf753
WIZIRAT <ni.kb@yandex.com>
2024-05-13 10:25:34 +0300

    fix core.py

--- brain_games/core.py
+++ brain_games/core.py
@@ -3,16 +3,17 @@ import prompt
 
 def game_even(question_and_answer, instruction):
     print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name?")
-    print(f"Hello, {name}!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!\n"
+          f"{instruction}")
     for i in range(3):
         question, correct_answer = question_and_answer()
         user_answer = prompt.string(f"Question: {question}\n"
-                               "Your answer:")
+                               "Your answer: ")
         if user_answer == correct_answer:
             print('Correct!')
         else:
-            print("{user_answer} is wrong answer ';('. Correct answer was {correct_answer}\n"
+            print(f"{user_answer} is wrong answer ';('. Correct answer was {correct_answer}\n"
                   f"Let's try again, {name}!")
             return
         print(f"Confratulations, {name}!")
