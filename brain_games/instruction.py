b8ca512c184c8e83b842b9d10bd8b01bb061a341
WIZIRAT <ni.kb@yandex.com>
2024-05-14 12:05:39 +0300

    fix instruction

--- brain_games/instruction.py
+++ brain_games/instruction.py
@@ -1,2 +1,3 @@
 INSTRUCTION_EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
 INSTRUCTION_CALC = 'What is the result of the expression?'
+INSTRUCTION_GCD = 'Find the greatest common divisor of given numbers.'
