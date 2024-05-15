d6bdee9272c558adb487a5140ba272b2e58b6e4d
WIZIRAT <ni.kb@yandex.com>
2024-05-15 09:43:37 +0300

    fix instruction

--- brain_games/instruction.py
+++ brain_games/instruction.py
@@ -1,3 +1,4 @@
 INSTRUCTION_EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
 INSTRUCTION_CALC = 'What is the result of the expression?'
 INSTRUCTION_GCD = 'Find the greatest common divisor of given numbers.'
+INSTRUCTION_PROGRESSION = 'What number is missing in the progression?'
