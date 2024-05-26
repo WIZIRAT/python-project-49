bc8e911060c085fe11e6068b68d2e1d5ea9606ac
WIZIRAT <ni.kb@yandex.com>
2024-05-26 09:54:41 +0300

    del instruction.py

--- brain_games/instruction.py
+++
@@ -1,6 +0,0 @@
-INSTRUCTION_EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
-INSTRUCTION_CALC = 'What is the result of the expression?'
-INSTRUCTION_GCD = 'Find the greatest common divisor of given numbers.'
-INSTRUCTION_PROGRESSION = 'What number is missing in the progression?'
-INSTRUCTION_PRIME = 'Answer "yes" if given number is prime. \
-Otherwise answer "no".'
