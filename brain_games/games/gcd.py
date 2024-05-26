fcd1a635b51478aeb3ac122e790ac6ab9d592703
WIZIRAT <ni.kb@yandex.com>
2024-05-26 09:48:37 +0300

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,14 +1,16 @@
 from brain_games.core import game_logic
 import random
-from brain_games.instruction import INSTRUCTION_GCD
 import math
+
+
+INSTRUCTION_GCD = 'Find the greatest common divisor of given numbers.'
 
 
 def find_gcd():
     num_one = random.randint(1, 100)
     num_second = random.randint(1, 100)
 
-    expression = (f"{num_one} {num_second}")
+    expression = (f'{num_one} {num_second}')
     result = str(math.gcd(num_one, num_second))
     return expression, result
 
