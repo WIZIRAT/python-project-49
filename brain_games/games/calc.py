84935800d93b6f4b7108c22702e68f7024ed403a
WIZIRAT <ni.kb@yandex.com>
2024-05-30 07:27:59 +0300

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,11 +2,13 @@ import random
 
 
 GAME_INSTRUCTION = 'What is the result of the expression?'
+MIN_NUM = 1
+MAX_NUM = 100
 
 
 def generate_game():
-    num_one = random.randint(1, 100)
-    num_second = random.randint(1, 100)
+    num_one = random.randint(MIN_NUM, MAX_NUM)
+    num_second = random.randint(MIN_NUM, MAX_NUM)
     operator = random.choice(['+', '-', '*'])
 
     expression = (f'{num_one} {operator} {num_second}')
@@ -14,6 +16,6 @@ def generate_game():
         result = num_one + num_second
     elif operator == '-':
         result = num_one - num_second
-    else:
+    elif operator == '*':
         result = num_one * num_second
     return expression, str(result)
