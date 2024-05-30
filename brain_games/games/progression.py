2cf8612f984a95e1266431198a540eadb126052a
WIZIRAT <ni.kb@yandex.com>
2024-05-30 10:16:44 +0300

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,15 +12,19 @@ START_NUM_HIDDEN_INDEX = 0
 
 
 def generate_game():
-    progression_len = random.randint(MIN_PROGRESSION_LEN, MAX_PROGRESSION_LEN)
-    hidden_index = random.randint(START_NUM_HIDDEN_INDEX, progression_len - 1)
-    start_number = random.randint(MIN_START_NUM, MAX_START_NUM)
-    step = random.randint(MIN_STEP_LEN, MAX_STEP_LEN)
-    progression = []
-
-    for i in range(progression_len):
-        progression.append(start_number + step * i)
     progression[hidden_index] = '..'
     result = ' '.join(map(str, progression))
     correct_answer = str(start_number + step * hidden_index)
     return result, correct_answer
+
+
+def generate_progression():
+    progression_len = random.randint(MIN_PROGRESSION_LEN, MAX_PROGRESSION_LEN)
+    hidden_index = random.randint(START_NUM_HIDDEN_INDEX, progression_len - 1)
+    start_number = random.randint(MIN_START_NUM, MAX_START_NUM)
+    step = random.randint(MIN_STEP_LEN, MAX_STEP_LEN)
+    progression = [
+    for i in range(progression_len):
+        progression.append(start_number + step * i)
+    ]
+    return progression
