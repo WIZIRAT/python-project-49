19cd98a92c23ecb506800bd6e545190a24a7882b
WIZIRAT <ni.kb@yandex.com>
2024-05-30 08:56:38 +0300

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,9 +9,18 @@ MAX_NUM = 100
 
 def generate_game():
     number = random.randint(MIN_NUM, MAX_NUM)
+    if is_prime(number) is False:
+        correct_answer = 'no'
+    else:
+        correct_answer = 'yes'
+    return number, correct_answer
+
+
+def is_prime(number):
     if number == 1 or 0:
-        return number, 'no'
+        return False
     for i in range(2, (number // 2 + 1)):
         if number % i == 0:
-            return number, 'no'
-    return number, 'yes'
+            return False
+    else:
+        return True
